616595ab8ca828532a502bf4b1a9e1674b59354d
udayshankar754 <uday.naxtre@gmail.com>
2024-06-17 18:31:14 +0530

    layout Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,16 @@
 import { Routes } from '@angular/router';
+import { EquityDataMasterComponent } from './equity-data-master/equity-data-master.component';
+import { LayoutComponent } from './component/layout/layout.component';
 
 export const routes: Routes = [
-  
+  {
+    path : '',
+    component : LayoutComponent,
+    children : [
+      {
+        path : 'equityMaster',
+        component : EquityDataMasterComponent
+      }
+    ]
+  },  
 ];
---
+++ src/app/component/layout/layout.component.ts
@@ -0,0 +1,25 @@
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet, 
+    NzIconModule,
+    NzLayoutModule,
+    NzMenuModule,
+    RouterLink,
+  ],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css'
+})
+export class LayoutComponent {
+  isCollapsed = false;
+
+}
