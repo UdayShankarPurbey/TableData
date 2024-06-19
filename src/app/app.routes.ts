8e9d66f8f927c50135b194998e68412b631c3f9b
udayshankar754 <uday.naxtre@gmail.com>
2024-06-19 18:28:40 +0530

    page not found Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { TopGainerComponent } from './pages/top-gainer/top-gainer.component';
 import { Routes } from '@angular/router';
 import { EquityDataMasterComponent } from './pages/equity-data-master/equity-data-master.component';
 import { LayoutComponent } from './component/layout/layout.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {
@@ -20,6 +21,10 @@ export const routes: Routes = [
       {
         path : 'topGainer',
         component : TopGainerComponent
+      },
+      {
+        path : '**',
+        component : PageNotFoundComponent
       }
     ]
   },  
